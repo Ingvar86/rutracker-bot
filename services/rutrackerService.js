962b1794da504eefef6d7a1e4fdcd3f149180c4e
ikac <ikac@isd.dp.ua>
2016-12-26 18:54:49 +0200

    Login and set cookie.

--- services/rutrackerService.js
+++ services/rutrackerService.js
@@ -5,69 +5,87 @@ var request = require('request'),
     topicService = require('./topicService'),
     EventEmitter = require('events'),
     baseUrl = 'http://rutracker.org/forum/',
-    rutracker_cookie = process.env.RUTRACKER_COOKIE;
+    loginUrl = baseUrl + 'login.php';
 
+function Rutracker(user, password, url) {
+    this.user = user;
+    this.password = password;
+    this.url = url;
+    this.login(user, password);
+}
 
-function Rutracker(url) {
-    var event = new EventEmitter();
-    this.fetch = function() {
-        return new Promise((resolve, reject) => {
-            const options = {
-                url: url,
-                encoding: null,
-                headers: {
-                    'Cookie': 'bb_data=' + rutracker_cookie + '; opt_js={"only_new":2}'
+Rutracker.prototype = new EventEmitter();
+
+Rutracker.prototype.login = function(user, password) {
+    const me = this;
+    const options = {
+        url: loginUrl,
+        form: {
+            login_username: user || me.user,
+            login_password: password || me.password,
+            login: 'Вход'
+        }
+    };
+    request.post(options, function (error, response, body) {
+        me.cookie = response.headers['set-cookie'][0];
+        me.emit('login');
+    });    
+};
+
+Rutracker.prototype.fetch = function() {
+    const me = this;
+    return new Promise((resolve, reject) => {
+        const options = {
+            url: me.url,
+            encoding: null,
+            headers: {
+                'Cookie': me.cookie + '; opt_js={"only_new":2}'
+            }
+        };
+        request(options, function (error, response, body) {
+            if (!error) {
+                var body1 = iconv.decode(body, 'win1251');
+                var $ = cheerio.load(body1),
+                    topics = $('tr[class="hl-tr"]'),
+                    topicsArray = [];
+                for (let i = 0; i < topics.length; i++){
+                    let elem = topics.eq(i);
+                    let a = elem.find('a.torTopic');
+                    let title = a.text();
+                    let href = a.attr('href');
+                    topicsArray.push({title: title, href: baseUrl + href});
                 }
-            };
-            request(options, function (error, response, body) {
-                if (!error) {
-                    var body1 = iconv.decode(body, 'win1251');
-                    var $ = cheerio.load(body1),
-                        topics = $('tr[class="hl-tr"]'),
-                        topicsArray = [];
-                    for (let i = 0; i < topics.length; i++){
-                        let elem = topics.eq(i);
-                        let a = elem.find('a.torTopic');
-                        let title = a.text();
-                        let href = a.attr('href');
-                        topicsArray.push({title: title, href: baseUrl + href});
-                    }
-                    event.emit('data', topicsArray);
-                    topicService.getTopics().then(result =>{
-                        let filtered = [];
-                        if (result) {
-                            let oldTopics = result.topics;
-                            filtered = topicsArray.filter((elem) => {
-                                return !oldTopics.find(old => {
-                                    return old.href === elem.href;
-                                });
+                me.emit('data', topicsArray);
+                topicService.getTopics().then(result =>{
+                    let filtered = [];
+                    if (result) {
+                        let oldTopics = result.topics;
+                        filtered = topicsArray.filter((elem) => {
+                            return !oldTopics.find(old => {
+                                return old.href === elem.href;
                             });
-                            if (filtered.length > 0) {
-                                event.emit('update', filtered);
-                                topicService.setTopics(topicsArray);
-                            }
-                        }
-                        else {
+                        });
+                        if (filtered.length > 0) {
+                            me.emit('update', filtered);
                             topicService.setTopics(topicsArray);
                         }
-                        resolve(filtered);
-                    });
-                } else {
-                    console.log('Error: ' + error);
-                    reject(error);
-                }
-            });
-        });        
-    };
+                    }
+                    else {
+                        topicService.setTopics(topicsArray);
+                    }
+                    resolve(filtered);
+                });
+            } else {
+                console.log('Error: ' + error);
+                reject(error);
+            }
+        });
+    });        
+};
 
-    this.start = function(interval) {
-        this.fetch();
-        setInterval(this.fetch, interval);         
-    };
-
-    this.on = function(name, callback) {
-        event.on(name, callback);
-    };
-}
+Rutracker.prototype.start = function(interval) {
+    this.fetch();
+    setInterval(this.fetch, interval);         
+};
 
 module.exports = Rutracker;
--- task.js
+++ task.js
@@ -2,21 +2,24 @@
 var Rutracker = require('./services/rutrackerService'),
     bot = require('./telegram/bot'),
     connectionService = require('./services/connectionService'),
-    url = 'http://rutracker.org/forum/viewforum.php?f=257';
+    url = 'http://rutracker.org/forum/viewforum.php?f=257',
+    user = process.env.USER,
+    password = process.env.PASSWORD;
 
+var rutracker = new Rutracker(user, password, url);
 
-var rutracker = new Rutracker(url);
-
-rutracker.fetch().then((newTopics) => {
-    if (newTopics && newTopics.length >0) {
-        var message = newTopics.reduce((prev, next) => {
-            return prev + next.title + '\n' + next.href + '\n'; 
-        }, '');
-        bot.notifyAll(message).then(()=> {
-            connectionService.closeConnection();
-        });
-    }
-    else {
-        connectionService.closeConnection();        
-    }
+rutracker.on('login', () => {
+    rutracker.fetch().then((newTopics) => {
+        if (newTopics && newTopics.length >0) {
+            var message = newTopics.reduce((prev, next) => {
+                return prev + next.title + '\n' + next.href + '\n'; 
+            }, '');
+            bot.notifyAll(message).then(()=> {
+                connectionService.closeConnection();
+            });
+        }
+        else {
+            connectionService.closeConnection();        
+        }
+    });
 });
