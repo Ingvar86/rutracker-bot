1e7dfc7c73d1a9e2ea08445c1348fb11afea4f32
Ingvar86 <gara314@gmail.com>
2020-11-08 22:12:36 +0200

    Correct mongodb close connection

--- services/connectionService.js
+++ services/connectionService.js
@@ -2,13 +2,18 @@ var MongoClient = require('mongodb').MongoClient;
 const url = process.env.DB_URL;
 
 var database;
+var mclient;
 
 exports.getConnection = function() {
-    return database ? Promise.resolve(database) : MongoClient.connect(url,  {useUnifiedTopology: true}).then(client => database = client.db());  
+    return database ? Promise.resolve(database) : MongoClient.connect(url,  {useUnifiedTopology: true}).then(client => {
+        mclient = client;
+        database = client.db();
+        return database;
+    });  
 };
 
 exports.closeConnection = function() {
     if (database) {
-        database.close();
+        mclient.close();
     }
 };
